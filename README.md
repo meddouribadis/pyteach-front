5b9d2612e540be15da190571255666de8ea38c9d
Badis Meddouri <meddouri.badis@gmail.com>
2020-10-01 10:12:33 +0200

    Header Conditional rendering

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# pyteach-front
+# PyTeach Front
+
+## Installation
+
+Clone this repo and open a terminal inside the folder, then type :
+
+`npm install`
+
+`npm start`
+
+## Redux Actions
+
+Main store is created in the file `./_helpers/store.js` that use the variable rootReducer who combines all stores created within the `./_reducers/` folder.
+
+ 
+
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 
-class Navbar extends React.Component {
+import { userActions } from '../../_actions';
 
-    constructor(props) {
-        super(props);
+function Navbar() {
+    const users = useSelector(state => state.users);
+    const user = useSelector(state => state.authentication.user);
+    const dispatch = useDispatch();
+
+    console.log(user);
+    useEffect(() => {
+    }, []);
+
+    function handleDeleteUser(id) {
+        dispatch(userActions.delete(id));
     }
 
-    componentDidMount() {
+    function UserInfo(){
+        if (user) {
+            return <a className="btn btn-outline-primary" href="#">{user.username}</a>;
+        }
+        return <a className="btn btn-outline-primary" href="#">S'inscrire</a>;
     }
 
-    render() {
-        return(
-            <div
-                className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-                <h5 className="my-0 mr-md-auto font-weight-normal">PyTeach</h5>
-                <nav className="my-2 my-md-0 mr-md-3">
-                    <a className="p-2 text-dark" href="#">Cours</a>
-                    <a className="p-2 text-dark" href="#">Enterprise</a>
-                    <a className="p-2 text-dark" href="#">Support</a>
-                    <a className="p-2 text-dark" href="#">Pricing</a>
-                </nav>
-                <a className="btn btn-outline-primary" href="#">S'inscrire</a>
-            </div>
-        )
-    }
+    return (
+        <div
+            className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+            <h5 className="my-0 mr-md-auto font-weight-normal">PyTeach</h5>
+            <nav className="my-2 my-md-0 mr-md-3">
+                <a className="p-2 text-dark" href="#">Cours</a>
+                <a className="p-2 text-dark" href="#">Enterprise</a>
+                <a className="p-2 text-dark" href="#">Support</a>
+                <a className="p-2 text-dark" href="#">Pricing</a>
+                <UserInfo />
+            </nav>
 
+        </div>
+    );
 }
 
 export { Navbar };
