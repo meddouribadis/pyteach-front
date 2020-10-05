72b172bc5511c49f219f8ae8e15134143c77e93e
Badis Meddouri <meddouri.badis@gmail.com>
2020-10-05 15:03:40 +0200

    Dynamic display of Courses

---
+++ src/Components/CourseInfobox/CourseInfobox.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function CourseInfobox(props) {
+
+    return (
+        <div className="card">
+            <h5 className="card-header">{props.course.title}</h5>
+            <div className="card-body">
+                <h5 className="card-title">{props.course.description}</h5>
+                <p className="card-text">{props.course.tags}</p>
+                <a href="#" className="btn btn-primary">Acceder au cours</a>
+            </div>
+        </div>
+    );
+}
+
+export { CourseInfobox };
