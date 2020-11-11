57aff48183f217eb14a514b79837b625c8a5d0dc
Badis MEDDOURI <meddouri.badis@gmail.com>
2020-11-11 15:21:57 +0100

    Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,42 +1,75 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { userActions } from '../../_actions';
+import React, {Fragment} from "react";
+import logo from '../../Assets/univ_evvry_logo.png';
+import {useSelector} from "react-redux";
 
 function Navbar() {
-    const users = useSelector(state => state.users);
+
     const user = useSelector(state => state.authentication.user);
-    const dispatch = useDispatch();
-
-    console.log(user);
-    useEffect(() => {
-    }, []);
-
-    function handleDeleteUser(id) {
-        dispatch(userActions.delete(id));
-    }
 
     function UserInfo(){
         if (user) {
-            return <a className="btn btn-outline-primary" href="/login">{user.username}</a>;
+            return (
+                <div className="dropdown show">
+                    <a className="py-2 d-none d-md-inline-block dropdown-toggle" href="#" role="button" id="dropdownMenuUser"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {user.firstName}
+                    </a>
+
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuUser">
+                        <a className="dropdown-item" href="/profile">Mon profil</a>
+                        <a className="dropdown-item" href="#">Contact</a>
+                        <a className="dropdown-item" href="/login">Se déconnecter</a>
+                    </div>
+                </div>
+            );
         }
-        return <a className="btn btn-outline-primary" href="/login">S'inscrire</a>;
+        return (
+            <Fragment>
+                <a className="py-2 d-none d-md-inline-block" href="/login">Connexion</a>
+                <a className="py-2 d-none d-md-inline-block" href="/register">Inscription</a>
+            </Fragment>
+        );
+    }
+
+    function TeacherLinks(){
+        return (
+            <Fragment>
+                <a className="py-2 d-none d-md-inline-block" href="/dashboard/classes">Mes classes</a>
+                <a className="py-2 d-none d-md-inline-block" href="/dashboard/signalements">Signalements</a>
+                <a className="py-2 d-none d-md-inline-block" href="/calendrier">Calendrier</a>
+            </Fragment>
+        );
     }
 
     return (
-        <div
-        class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <h5 className="my-0 mr-md-auto font-weight-normal"><h5 class="table-dark" href="#">PyTeach</h5></h5>
-            <nav className="my-2 my-md-0 mr-md-3">
-                <a class="table-dark" href={"/courses"}>Cours</a>&nbsp;&nbsp;
-                <a class="table-dark" href="/">Présentation</a>&nbsp;&nbsp;
-                <a class="table-dark" href="#">Support</a>&nbsp;&nbsp;
-                <a class="table-dark" href="#">Pricing</a>&nbsp;&nbsp;
-                <UserInfo />
-            </nav>
+        <nav className="site-header sticky-top">
+            <div className="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+                <a className="navbar-brand" href="/">
+                    <img src={logo} alt="" className={"img-fluid logo"}/>
+                </a>
 
-        </div>
+                <a className="top-link py-2 d-none d-md-inline-block" href="/">Accueil</a>
+
+                {user && user.role !== 'TEACHER' &&
+                <div className="dropdown show">
+                    <a className="py-2 d-none d-md-inline-block dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Signalements</a>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a className="dropdown-item" href="/signalement/">Mes signalements</a>
+                        <a className="dropdown-item" href="/signalement/infection">Je suis infecté</a>
+                        <a className="dropdown-item" href="/signalement/cas-contact">Je suis cas contact</a>
+                    </div>
+                </div>
+                }
+
+
+                {user && user.role === 'TEACHER' &&
+                <TeacherLinks/>
+                }
+
+                <a className="top-link py-2 d-none d-md-inline-block" href="#">Information</a>
+                <UserInfo/>
+            </div>
+        </nav>
     );
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ]
             }
         ]
     },
