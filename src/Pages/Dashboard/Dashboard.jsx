17657af92dd56a48c8e25db5f606014c55621b21
Badis <meddouri.badis@gmail.com>
2020-11-25 18:01:58 +0100

    Design

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { Switch, Link, Route, useParams, useRouteMatch} from 'react-router-dom';
+import './Dashboard.css';
 
 //Routes
 import {CreateCourse, EditCourse, ManageCourses} from "./CourseDashboard";
@@ -37,11 +38,73 @@ function DashboardHome() {
                     <h1>Tableau de bord !</h1>
                     <hr/>
                     <p>Vous pouvez réalisez les actions suivantes :</p>
-                    <Link to={{pathname: "/dashboard/course/create"}} className="btn btn-link">Créer un cours</Link>
-                    <Link to={{pathname: "/dashboard/course/manage"}} className="btn btn-link">Gérer les cours</Link>
-                    <Link to={{pathname: "/dashboard/category/create"}} className="btn btn-link">Créer une catégorie</Link>
-                    <Link to={{pathname: "/dashboard/category/manage"}} className="btn btn-link">Gérer les catégories</Link>
                 </div>
+            </div>
+            <div className="row">
+
+                <div className="col-sm-6 col-md-6 col-lg-3">
+                    <div className="card-stats card">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="title text-center">
+                                        <h4 className="card-category">Cours</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><Link to={{pathname: "/dashboard/course/create"}} className="btn btn-link">Créer un cours</Link></li>
+                            <li className="list-group-item"><Link to={{pathname: "/dashboard/course/manage"}} className="btn btn-link">Gérer les cours</Link></li>
+                        </ul>
+                        <div className="card-footer">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-6 col-md-6 col-lg-3">
+                    <div className="card-stats card">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="title text-center">
+                                        <h4 className="card-category">Catégories</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><Link to={{pathname: "/dashboard/category/create"}} className="btn btn-link">Créer une catégorie</Link></li>
+                            <li className="list-group-item"><Link to={{pathname: "/dashboard/category/manage"}} className="btn btn-link">Gérer les catégories</Link></li>
+                        </ul>
+                        <div className="card-footer">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-sm-6 col-md-6 col-lg-3">
+                    <div className="card-stats card">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-12">
+                                    <div className="title text-center">
+                                        <h4 className="card-category">Articles</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                            <li className="list-group-item"><Link to={{pathname: "/dashboard/article/create"}} className="btn btn-link">Créer un article</Link></li>
+                            <li className="list-group-item"><Link to={{pathname: "/dashboard/article/manage"}} className="btn btn-link">Gérer les articles</Link></li>
+                        </ul>
+                        <div className="card-footer">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
 
